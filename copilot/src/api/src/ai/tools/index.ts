27a8e9f429a88cf728b8c7897764dc8879d34bba
Daniel Fox <danny.fox97@hotmail.com>
2025-06-19 09:15:31 +0100

    move api into monorepo project

---
+++ copilot/src/api/src/ai/tools/index.ts
@@ -0,0 +1,7 @@
+import { PdfTool } from './pdf_tool';
+import { PirateTool } from './pirate_tool';
+import { ResearchPdfTool } from './research_pdf_tool';
+import { SearchTool } from './web-search_tool';
+import { WikiTool } from './wiki_tool';
+
+export { PdfTool, PirateTool, ResearchPdfTool, SearchTool, WikiTool };
